f3b7615aee49768498834ce843297de362563418
MosesOpiyo <mosesopiyo54@gmail.com>
2024-02-21 12:41:19 +0300

    Notifications application changes

--- Notifications/models.py
+++ Notifications/models.py
@@ -5,6 +5,7 @@ class Notification(models.Model):
     message = models.TextField(blank=True)
     seen = models.BooleanField(default=False)
     date_added = models.DateTimeField(default=datetime(2023, 1, 1, 12, 0, 0))
+    route = models.TextField(blank=True)
 
     def __str__(self):
         return self.message
